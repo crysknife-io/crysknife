434c23b2591fd75986eebef6e56c7e0e0fd0e3d2
Dmitrii Tikhomirov <chani.liet@gmail.com>
2021-10-07 23:59:50 -0400

    ui: databinder:  now it's possible to use is/get getters from boolean fields

--- ui/databinding/generator/src/main/java/io/crysknife/ui/databinding/generator/BindableGenerator.java
+++ ui/databinding/generator/src/main/java/io/crysknife/ui/databinding/generator/BindableGenerator.java
@@ -127,7 +127,7 @@ public class BindableGenerator extends ScopedBeanGenerator {
 
     if (!errors.isEmpty()) {
       printErrors(errors);
-      throw new GenerationException(getClass().getCanonicalName() + ".generate");
+      throw new GenerationException("at " + beanDefinition.getType() + " during Databining generation");
     }
 
     maybeAddDefaultConverters(clazz, methodDeclaration);
