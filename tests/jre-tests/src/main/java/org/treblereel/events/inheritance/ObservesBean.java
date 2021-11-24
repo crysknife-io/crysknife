09c347fbf3d1a917e7696588e223168c4e695b52
Dmitii Tikhomirov <chani.liet@gmail.com>
2021-11-23 22:16:52 -0800

    core: events: @Observes inheritance tests

---
+++ tests/jre-tests/src/main/java/org/treblereel/events/inheritance/ObservesBean.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright © 2021 Treblereel
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package org.treblereel.events.inheritance;
+
+import org.treblereel.events.SimpleEvent;
+
+import javax.enterprise.event.Event;
+import javax.enterprise.event.Observes;
+import javax.inject.Inject;
+
+public class ObservesBean extends ObservesParent {
+
+  public Event<SimpleEvent> event;
+
+  @Inject
+  public ObservesBean(Event<SimpleEvent> event) {
+    this.event = event;
+  }
+
+  public void onEvent(@Observes SimpleEvent event) {
+    events.put(getClass(), event);
+  }
+}
